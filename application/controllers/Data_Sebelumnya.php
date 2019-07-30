61fe42c405875e75218fcda8dd43ea3f4b23b322
Mujahid97 <33047680+Mujahid97@users.noreply.github.com>
2019-07-30 08:26:26 +0700

    case sensitive fix

--- application/controllers/Data_Sebelumnya.php
+++ application/controllers/Data_Sebelumnya.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Data_Sebelumnya extends CI_Controller {
+class data_sebelumnya extends CI_Controller {
 
 	public function index()
 	{
--- application/controllers/Data_Validasi.php
+++ application/controllers/Data_Validasi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Data_Validasi extends CI_Controller {
+class data_validasi extends CI_Controller {
 
 	public function index()
 	{

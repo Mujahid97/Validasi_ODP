6b8c6a3a2b68cb580fb89f763aacc303db796ffb
Mujahid97 <33047680+Mujahid97@users.noreply.github.com>
2019-07-30 08:30:05 +0700

    CaseFIX

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -29,38 +29,38 @@
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?php echo site_url('data_sebelumnya') ?>" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="<?php echo site_url('Data_Sebelumnya') ?>" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
           <span>Data Sebelumnya</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Daftar STO</h6>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/DMO') ?>">DMO</a>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/GBG') ?>">GBG</a>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/IJK') ?>">IJK</a>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/JGR') ?>">JGR</a>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/MYR') ?>">MYR</a>
-            <a class="collapse-item" href="<?php echo site_url('data_sebelumnya/RKT') ?>">RKT</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/DMO') ?>">DMO</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/GBG') ?>">GBG</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/IJK') ?>">IJK</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/JGR') ?>">JGR</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/MYR') ?>">MYR</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Sebelumnya/RKT') ?>">RKT</a>
           </div>
         </div>
       </li>
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?php echo site_url('data_validasi') ?>" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+        <a class="nav-link collapsed" href="<?php echo site_url('Data_Validasi') ?>" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i class="fas fa-fw fa-wrench"></i>
           <span>Data Proses Validasi</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
           <h6 class="collapse-header">Daftar STO</h6>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/DMO') ?>">DMO</a>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/GBG') ?>">GBG</a>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/IJK') ?>">IJK</a>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/JGR') ?>">JGR</a>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/MYR') ?>">MYR</a>
-            <a class="collapse-item" href="<?php echo site_url('data_validasi/RKT') ?>">RKT</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/DMO') ?>">DMO</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/GBG') ?>">GBG</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/IJK') ?>">IJK</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/JGR') ?>">JGR</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/MYR') ?>">MYR</a>
+            <a class="collapse-item" href="<?php echo site_url('Data_Validasi/RKT') ?>">RKT</a>
           </div>
         </div>
       </li>      
